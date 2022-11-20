c5d22ba9b5d28b4fed143dfe663b2089fd27340f
Taoufiq BAHALLA <taoufiq.bahalla@hotmail.com>
2022-11-20 15:00:57 +0100

    update message

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,21 @@ import './App.css';
 const socket = new WebSocket("ws://192.168.1.8:9090/ws");
 
 function App() {
-  const [message, setMessage] = useState('')
-  const [inputValue, setInputValue] = useState('')
+  const [message, setMessage] = useState({id: '', user: '', content: ''})
+  const [messageContent, setMessageContent] = useState('')
+  const [messageUser, setMessageUsert] = useState('')
 
   useEffect(() => {
     socket.onopen = () => {
-      setMessage('Connected')
+      setMessage({id: '', user: '', content: 'Connected !'})
     };
 
     socket.onmessage = (e) => {
-      setMessage("Get message from server: " + e.data)
+      setMessage(e.data)
     };
 
     socket.onclose = () => {
-      setMessage('Closed')
+      setMessage({id: '', user: '', content: 'Closed !' })
       socket.close()
     }
   }, [])
@@ -26,19 +27,34 @@ function App() {
     e.preventDefault()
 
     socket.send(JSON.stringify({
-      message: inputValue
+      id: Math.random() * 10, 
+      user: messageUser,
+      content: messageContent
     }))
-  }, [inputValue])
+  }, [messageContent, messageUser])
 
-  const handleChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(e.target.value)
+  const handleChangeContent = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessageContent(e.target.value)
+  }, [])
+  const handleChangeUser = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
+    setMessageUsert(e.target.value)
   }, [])
 
   return (
     <div className="App">
-      <input id="input" type="text" value={inputValue} onChange={handleChange} />
+      <label>
+        message: 
+        <input id="input" type="text" value={messageContent} onChange={handleChangeContent} />
+      </label>
+      <br/>
+      <label>
+        user: 
+        <input id="input" type="text" value={messageUser} onChange={handleChangeUser} />
+      </label>
+      <br/>
       <button onClick={handleClick}>Send</button>
-      <pre>{message}</pre>
+   
+      <pre>{message.content}</pre>
     </div>
   );
 }
